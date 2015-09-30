6ca00e3a6540a2d70c35ac5b4a8ad8c832c86e39
louwii <link14.online@gmail.com>
2015-09-29 23:05:35 -0700

    fix issue not using todo-list-item; add todo count

--- app/models/todo-list.js
+++ app/models/todo-list.js
@@ -3,5 +3,12 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  todoItems: DS.hasMany('todo-item')
+  todoItems: DS.hasMany('todo-item'),
+
+  nbItems: Ember.computed('todoItems', function() {
+    return this.get('todoItems').length;
+  }),
+  nbItemsUncomplete: Ember.computed('todoItems', function() {
+    return 0;
+  })
 });
--- app/routes/todo-lists.js
+++ app/routes/todo-lists.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    // findAll() do a query, peekAll() will not
     var todoLists = this.store.findAll('todo-list');
-    console.log(todoLists);
+    // console.log(todoLists);
     return todoLists;
   },
   actions: {
