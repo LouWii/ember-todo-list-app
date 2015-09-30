b764850c82d361cb3f422c32ff4d5636f985be1f
louwii <link14.online@gmail.com>
2015-09-29 22:10:54 -0700

    main style for todo-list single;back button action

--- app/routes/todo-list.js
+++ app/routes/todo-list.js
@@ -4,7 +4,16 @@ export default Ember.Route.extend({
   model(params) {
     // findRecord() will make a new request, peekRecord() will not
     var todoList = this.store.peekRecord('todo-list', params.todo_list_id);
+    if (todoList === null) {
+      // If todo-list not found, try to get it with a query
+      todoList = this.store.findRecord('todo-list', params.todo_list_id);
+    }
     console.log(todoList);
     return todoList;
+  },
+  actions: {
+    goBack() {
+      this.transitionTo('todo-lists');
+    }
   }
 });
