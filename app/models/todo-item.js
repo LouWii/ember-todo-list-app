da9629e40026f1b2e8303930ee6036b901ff218c
louwii <link14.online@gmail.com>
2015-09-29 22:47:44 -0700

    add default values for creationDate and isComplet

--- app/models/todo-item.js
+++ app/models/todo-item.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  creationDate: DS.attr('date'),
-  isCompleted: DS.attr('boolean'),
+  creationDate: DS.attr('date', { defaultValue() { return new Date(); } }),
+  isCompleted: DS.attr('boolean', { defaultValue() { return false; } }),
   todoList: DS.belongsTo('todo-list')
 });
