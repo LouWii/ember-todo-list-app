c181d816825c8749b590a4ec662692398d70e3c3
louwii <link14.online@gmail.com>
2015-09-29 20:54:34 -0700

    Use peekRecord to avoid doing a new query

--- app/routes/todo-list.js
+++ app/routes/todo-list.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     // findRecord() will make a new request, peekRecord() will not
-    var todoList = this.store.findRecord('todo-list', params.todo_list_id);
+    var todoList = this.store.peekRecord('todo-list', params.todo_list_id);
     console.log(todoList);
     return todoList;
   }
