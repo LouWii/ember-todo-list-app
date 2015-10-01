9ad217b84f0128c4549f5a623ab26ff0175d94e8
louwii <link14.online@gmail.com>
2015-09-30 20:59:31 -0700

    avoid deprecated synchronous relationship

--- app/models/todo-list.js
+++ app/models/todo-list.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  todoItems: DS.hasMany('todo-item'),
+  todoItems: DS.hasMany('todo-item', { async: false }),
 
   nbItems: Ember.computed('todoItems', function() {
     return this.get('todoItems').length;
